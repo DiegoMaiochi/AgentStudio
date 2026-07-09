9df1eab52261ac632a03e79c1ce8ad84ccf4249d
Diego Maiochi <diegoabreu382@gmail.com>
2026-07-09 17:38:21 -0300

    configura rotas para demais telas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./components/Dashboard";
+import Projetos from "./components/Projetos";
+import Atualizacoes from "./components/Atualizacoes";
 
 function App() {
   return (
@@ -8,8 +10,13 @@ function App() {
       <div className="border-r border-r-gray-800">
         <Sidebar />
       </div>
+
       <div className="flex-1 flex items-center justify-center">
-        <Dashboard />
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projetos" element={<Projetos />} />
+          <Route path="/atualizacoes" element={<Atualizacoes />} />
+        </Routes>
       </div>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,39 @@
-import { Features } from "tailwindcss";
 import logo from "../assets/logo.png";
-import Dashboard from "./Dashboard";
-import Projetos from "./Projetos";
-import Atualizacoes from "./Atualizacoes";
 import { useNavigate } from "react-router-dom";
 
-function Sidebar(){
-
+function Sidebar() {
     const navigate = useNavigate();
 
-    return(
-        <div className="h-screen flex flex-col items-center mx-14">
+    return (
+        <div className="h-screen w-60 flex flex-col items-center">
             <div className="mt-4">
-                <img src={logo} alt="Logo" className="w-35" />
+                <img onClick={() => navigate("/Dashboard")} src={logo} alt="Logo" className="w-35 cursor-pointer" />
             </div>
 
-           <div className="mt-52 flex flex-col items-center space-y-2">
-                <button onClick={() => navigate (<Dashboard />)}  className="px-3 py-1 hover:bg-gray-500 hover:scale-110 transition duration-300 rounded-lg cursor-pointer">Dashboard</button>
+            <div className="mt-52 flex flex-col items-center space-y-2">
+                <button
+                    onClick={() => navigate("/Dashboard")}
+                    className="px-3 py-1 hover:scale-120 transition duration-200 rounded-lg cursor-pointer"
+                >
+                    Dashboard
+                </button>
 
-                <button onClick={() => navigate (<Projetos />)} className="px-3 py-1 hover:bg-gray-500 hover:scale-110 transition duration-300 rounded-lg cursor-pointer">Projetos</button>
+                <button
+                    onClick={() => navigate("/projetos")}
+                    className="px-3 py-1 hover:scale-120 transition duration-200 rounded-lg cursor-pointer"
+                >
+                    Projetos
+                </button>
 
-                <button onClick={() => navigate (<Atualizacoes />)} className="px-3 py-1 hover:bg-gray-500 hover:scale-110 transition duration-300 rounded-lg cursor-pointer">Atualizações</button>
+                <button
+                    onClick={() => navigate("/atualizacoes")}
+                    className="px-3 py-1 hover:scale-120 transition duration-200 rounded-lg cursor-pointer"
+                >
+                    Atualizações
+                </button>
             </div>
         </div>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
