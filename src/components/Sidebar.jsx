6f3a0a917f1a33a57f6cefdfba07d076cd1fe9ee
Diego Maiochi <diegoabreu382@gmail.com>
2026-07-09 11:40:33 -0300

    atualiza estilização da Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,17 @@
+import logo from "../assets/logo.png";
+
 function Sidebar(){
     return(
-        <div className="m-17">
-            <p className="p-4">Dashboard</p>
-            <p className="p-4">Projetos</p>
-            <p className="p-4">Features</p>
+        <div className="h-screen flex flex-col items-center mx-14">
+            <div className="mt-4">
+                <img src={logo} alt="Logo" className="w-35" />
+            </div>
+
+            <div className="mt-52 text-center">
+                <p className="p-2">Dashboard</p>
+                <p className="p-2">Projetos</p>
+                <p className="p-2">Features</p>
+            </div>
         </div>
     )
 }
